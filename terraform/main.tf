126cc9e786e23d64bdcab29531ce480b1c0b86bc
pushpak-23 <pushpakithule@gmail.com>
2025-02-02 06:46:07 +0000

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "google" {
+  project = "your-gcp-project"
+  region  = "us-central1"
+}
