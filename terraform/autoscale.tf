8c88950bb27353fcf598765af2686eb524453c40
Pushpak Ithule <70135309+pushpak-23@users.noreply.github.com>
2025-02-02 14:43:18 +0530

    Delete terraform directory

--- terraform/autoscale.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_autoscaling_policy" "hpc_scale" {
-  name                   = "hpc-autoscale"
-  scaling_adjustment     = 2
-  adjustment_type        = "ChangeInCapacity"
-  autoscaling_group_name = aws_autoscaling_group.hpc.name
-}
--- terraform/aws_hpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "hpc_node" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "c5.9xlarge"
-  count         = 2
-  tags = {
-    Name = "hpc-node-${count.index}"
-  }
-}
