4158064faff195b6ac3eb63e5e3c9a9a72bdcf81
Ramerwararyan <162675236+Ramerwararyan@users.noreply.github.com>
2025-02-25 01:14:41 +0530

    Update migrate_job.sh

--- migrate_job.sh
+++ migrate_job.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+AWS_BUCKET="s3://slurm-checkpoints"
+
 echo "Checkpointing running SLURM jobs..."
 for job in $(squeue --format="%A" --state=RUNNING); do
     scontrol checkpoint create JobId=$job
 done
 
-echo "Transferring checkpoint files to AWS..."
-scp -r /checkpoint/ ec2-user@aws-slurm-master:/checkpoint/
-
-echo "Resuming jobs on AWS cluster..."
-ssh ec2-user@aws-slurm-master "sbatch /checkpoint/restart_jobs.sh"
+echo "Syncing checkpoint files to S3..."
+aws s3 sync /checkpoint/ $AWS_BUCKET --storage-class STANDARD
