d6a552751f6f16785a1fac27a6be55e56e45ea06
Ramerwararyan <162675236+Ramerwararyan@users.noreply.github.com>
2025-02-25 01:16:35 +0530

    Update restart_job.sh

--- restart_job.sh
+++ restart_job.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+AWS_BUCKET="s3://slurm-checkpoints"
+
+echo "Syncing checkpoint files from S3..."
+aws s3 sync $AWS_BUCKET /checkpoint/
+
 echo "Restarting jobs from checkpoint..."
 for checkpoint in /checkpoint/*.ckpt; do
     scontrol restart $checkpoint
